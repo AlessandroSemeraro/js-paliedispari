dd29bf7adc58836b76abdfc9d378ec88f01c07c4
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-11-21 09:40:30 +0100

    added functions and conditions

--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -1,12 +1,38 @@
+//Pari e Dispari:
 //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-let userChoiceDrawOrEven = prompt('draw or even?');
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+//Sommiamo i due numeri
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando un'altra funzione)
+//Dichiariamo chi ha vinto.
 
-let userChoiceNumber = prompt('type a number from 1 to 5');
-
-//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
-function botRandomNumber (botNumber){
-    let botBoh = Math.floor( Math.random() * 4 ) +1;
-    alert('You rolled a ' + botNumber);
+let userChooseEvenOrOdd = parseInt(prompt('even or odd'));
+while (userChooseEvenOrOdd != 1 || userChooseEvenOrOdd !=2){
+    
 }
 
-botRandomNumber
+let userChooseRandomNumber =parseInt(prompt('choose a number from 1 to 5'));
+
+function randomNumberBot (){
+    let numberBot = Math.round(Math.random()*5)+1;
+    return numberBot
+}
+
+let randomNumber= randomNumberBot()
+
+let sumNumbers= userChooseRandomNumber + randomNumber;
+
+function evenOrOdd (n){
+    if(n % 2 == 0){
+        console.log(n + ' pari')
+        return true
+    } else {
+        console.log(n + ' dispari')
+        return false
+    }
+}
+
+console.log(evenOrOdd(sumNumbers))
+
+
+ winnerIs 
+ if( userChooseEvenOrOdd)
